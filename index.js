df9b07eb56c2a6168523bee80c3a53ba6d02adf9
Luis David <lgloria07@alumnos.uaq.mx>
2025-09-08 20:10:10 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+/*
+VERBOS HTTP
+GET: Obtener datos 
+POST: Guardar datos
+PATCH: Actualizar un dato (Ej.nombre)
+PUT: Actualizar todos los elementos (Ej. nombre, apellido, edad)
+DELETE: Eliminar datos
+*/
+app.listen(3000, () => {
+
+    console.log("Server is running...");
+});
+
+app.get('/', (req, res, next) => {
+    res.status(200);
+    res.send('Hello World');
+});
