57b29458795d750d26d5ddaea7f280d191feb00e
LD7-coder <vagabunditoluisdavis@gmail.com>
2025-10-24 09:52:48 -0600

    Actividad 9

--- Routes/pokemon.js
+++ Routes/pokemon.js
@@ -18,6 +18,47 @@ pokemon.post("/",async (req,res,next) =>{
     return res.status(500).json({code:500,message:"Campos incompletos"});
 });
 
+pokemon.delete(/^\/(\d{1,3})$/, async (req,res,next) =>{
+    const query = `DELETE FROM pokemon WHERE pok_id = ${req.params[0]}`;
+    const rows = await db.query(query);
+
+    if(rows.affectedRows){
+        return res.status(200).json({code:200,message:"Pokemon borrado correctamente"});
+    }
+    return res.status(404).json({code:404,message:"Pokemon no encontrado"});
+});
+
+pokemon.put(/^\/(\d{1,3})$/,async(req,res,next)=>{
+    const {pok_name,pok_height,pok_weight,pok_base_experience} = req.body
+
+    if(pok_name && pok_height && pok_weight && pok_base_experience){
+        let query = `UPDATE pokemon SET pok_name='${pok_name}',pok_height=${pok_height},`;
+        query += `pok_weight=${pok_weight},pok_base_experience=${pok_base_experience} WHERE pok_id = ${req.params[0]};`;
+
+        const rows = await db.query(query)
+        if(rows.affectedRows == 1){
+            return res.status(200).json({code:200, message:"Pokemon actualizado correctarmente"});
+    }
+        return res.status(500).json({code: 500,message:"Ocurrio un error"});
+    }
+    return res.status(500).json({code:500,message:"Campos incompletos"});
+
+});
+
+pokemon.patch(/^\/(\d{1,3})$/,async(req,res,next)=>{
+
+    if(req.body.pok_name){
+        let query = `UPDATE pokemon SET pok_name='${req.body.pok_name}' WHERE pok_id=${req.params[0]}`
+
+        const rows = await db.query(query)
+        if(rows.affectedRows == 1){
+            return res.status(200).json({code:200, message:"Pokemon actualizado correctarmente"});
+    }
+    return res.status(500).json({code:500, message: "Ocurrio un error"});
+}
+    return res.status(500).json({code:500,message:"Campos incompletos"})
+});
+
 pokemon.get("/", async (req, res) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
     res.status(201).json({code: 201, message: pkmn});
@@ -32,7 +73,7 @@ pokemon.get(/^\/(\d{1,3})$/, async (req, res) => {
     (rows.length === 0) ? 
         res.status(404).send({code: 404, message: "El pokemon no existe"}) 
         : res.status(201).json({code: 201, message: rows[0]});
-    });
+});
 
 pokemon.get(/^\/([A-Za-z]+)$/, async (req, res) => {
     const name = req.params[0].toLowerCase();
