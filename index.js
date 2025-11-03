26ca1b6c3492c6d53aa0fd41a9f4949c720eccd6
LD7-coder <vagabunditoluisdavis@gmail.com>
2025-11-03 10:51:43 -0600

    Practica 12

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
+/*DEPENDENCIES*/
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
+/*ROUTERS*/
 const pokemon = require('./Routes/pokemon')
 const user = require("./Routes/user");
+/*MIDDLEWARE*/
+const auth = require('./middleware/auth');
+const notFound = require ('./middleware/notFound');
+const index = require('./middleware/index');
 
 app.use(morgan('dev')); 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+app.get('/',index );
 
-/*
-VERBOS HTTP
-GET: Obtener datos 
-POST: Guardar datos
-PATCH: Actualizar un dato (Ej. nombre)
-PUT: Actualizar todos los elementos (Ej. nombre, apellido, edad)
-DELETE: Eliminar datos
-*/
-app.get('/', (req, res) => {
-    res.status(200).json({code: 1, message: "Bienvenido al pokedex"});
-});
-
-app.use("/pokemon", pokemon);
 app.use("/user",user);
+app.use(auth);
+app.use("/pokemon", pokemon);
 
+app.use(notFound);
 
-app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "URL no encontrada"}); 
-    });
 
 app.listen(process.env.PORT || 3000, () => { 
     console.log("Server is running in port 3000");
